9b773143db81cdf51f07fb04d191727ce62dbde3
SeeMens <41332442+SeeMens@users.noreply.github.com>
2018-07-18 14:10:32 +0200

    Add files via upload

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,36 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks {
     public class RankedGradeBook : BaseGradeBook {
         public RankedGradeBook(string name) : base(name) {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade) {
+            char result;
+            if (Students.Count < 5) {
+                throw new InvalidOperationException("Ranked need at least 5 Students");
+            } else {            
+                var temp = (int)Math.Ceiling(Students.Count * 0.2);
+                var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+                if (grades[temp - 1] <= averageGrade) {
+                    result = 'A';
+                } else if (grades[2 * temp - 1] <= averageGrade) {
+                    result = 'B';
+                } else if (grades[3 * temp - 1] <= averageGrade) {
+                    result = 'C';
+                } else if (grades[4 * temp - 1] <= averageGrade) {
+                    result = 'D';
+                }
+
+            }
+            result = 'F';
+            return result;
         }
     }
 }
